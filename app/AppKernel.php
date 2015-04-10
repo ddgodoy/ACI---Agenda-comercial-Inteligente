ea2e8397141e5a22ce0ff3556edfcb75d6cd7c96
diole <diole@prueba.com>
2015-04-10 03:30:07 -0500

    cambios en ACI

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,7 @@ class AppKernel extends Kernel {
             new ACI\BackendBundle\BackendBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'prod', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
